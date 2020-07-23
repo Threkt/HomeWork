4bfdc5787cc52fb559b74b5c9efc5e8d1f1a4c20
Threkt <541068349@qq.com>
2020-07-23 20:31:51 +0800

    修改了XXLPlane

--- day12/src/com/zzxx/shoot/XXLPlane.java
+++ day12/src/com/zzxx/shoot/XXLPlane.java
@@ -1,7 +1,9 @@
 package com.zzxx.shoot;
 
 import java.awt.image.BufferedImage;
-
+/*
+这是超大号飞机
+* */
 public class XXLPlane extends FlyingObject implements Enemy, Award {
     protected XXLPlane(int x, int y, BufferedImage img, int life) {
         super(x, y, img, life);
