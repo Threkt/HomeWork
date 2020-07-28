1da711ac10bbd3b45f09a5626ac8f781fb741cc9
Threkt <541068349@qq.com>
2020-07-28 08:45:38 +0800

    第四次提交

--- HomeWork/src/day05/Test04.java
+++ HomeWork/src/day05/Test04.java
@@ -1,4 +1,8 @@
 package day05;
+
+import java.awt.image.ImageProducer;
+import java.util.*;
+
 /**
  *创建一个Map，保存某个学生的成绩:
  *在控制台输入该学生成绩，格式:
@@ -14,5 +18,43 @@ package day05;
  *
  */
 public class Test04 {
+    public static void main(String[] args) {
+        Map<String,Integer> map = new HashMap<>();
+        Scanner console = new Scanner(System.in);
+        System.out.println("请输入5门学生成绩，例：语文:99;数学:98;英语:97;物理:96;化学:95");
+        String str = console.next();
+        String[] split = str.split("\\;");
+        for (int i = 0; i < split.length; i++) {
+            String[] split1 = split[i].split("\\:");
+            map.put(split1[0],Integer.valueOf(split1[1]));
+        }
+        System.out.println(map.get("物理") );
+        map.put("化学",96);
+        map.remove("英语");
+
+
+        //1.遍历key
+        Set<String> set = map.keySet();
+        for (String s : set) {
+            System.out.println(s+":"+map.get(s));
+        }
+
+        System.out.println("===========================");
+
+        //2.遍历Entry
+        Set<Map.Entry<String, Integer>> set1 = map.entrySet();
+        for (Map.Entry<String, Integer> entry : set1) {
+            System.out.println(entry.getKey()+":"+entry.getValue());
+        }
+
+        System.out.println("===========================");
+
+        //3.遍历value
+        Collection<Integer> values = map.values();
+        for (Integer value : values) {
+            System.out.println(value);
+        }
+
+    }
 
 }
--- HomeWork/src/day05/Test05.java
+++ HomeWork/src/day05/Test05.java
@@ -1,4 +1,9 @@
 package day05;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
 /**
  * 有下列字符串:
  * 销售:张三;财务:李四;销售:王五;财务:赵六;程序:mike;程序:jerry;美工:jackson;前端:green;前端:nick;程序:钱七;销售:alice
@@ -9,5 +14,22 @@ package day05;
  *
  */
 public class Test05 {
-	 
+    public static void main(String[] args) {
+        String str = "销售:张三;财务:李四;销售:王五;财务:赵六;程序:mike;程序:jerry;美工:jackson;前端:green;前端:nick;程序:钱七;销售:alice";
+        Map<String, Integer> map = new HashMap<>();
+        String[] split = str.split("\\;");
+        for (int i = 0; i < split.length; i++) {
+            String[] split1 = split[i].split("\\:");
+            Integer integer = map.get(split1[0]);
+            if (integer==null){
+                integer=0;
+            }
+            map.put(split1[0],integer+1);
+        }
+        Set<String> set = map.keySet();
+        for (String s : set) {
+            System.out.println(s+":"+map.get(s));
+        }
+
+    }
 }
